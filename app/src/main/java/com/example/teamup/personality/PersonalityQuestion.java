ed176d025c442eaa0517cbfb41e695bab69e72b3
seo003 <sy030212@naver.com>
2025-08-14 20:01:11 +0900

    refactor: 성향테스트 질문 로딩 로직 분리 및 UI 수정

--- app/src/main/java/com/example/teamup/personality/PersonalityQuestion.java
+++ app/src/main/java/com/example/teamup/personality/PersonalityQuestion.java
@@ -44,7 +44,22 @@ public class PersonalityQuestion {
         return optionB;
     }
 
+    /**
+     * 질문 데이터를 로드하는 메서드
+     * 현재는 더미 데이터를 사용하지만, 나중에 백엔드에서 받아올 예정
+     */
     public static List<PersonalityQuestion> loadQuestions(Context context) {
+        // TODO: 백엔드에서 질문 데이터를 받아올 예정
+        // return loadQuestionsFromAPI();
+        
+        // 임시로 더미 데이터 사용
+        return loadDummyQuestions(context);
+    }
+
+    /**
+     * 더미 질문 데이터를 로드하는 메서드
+     */
+    public static List<PersonalityQuestion> loadDummyQuestions(Context context) {
         List<PersonalityQuestion> questions = new ArrayList<>();
 
         try {
@@ -70,9 +85,32 @@ public class PersonalityQuestion {
             }
 
         } catch (IOException | JSONException e) {
-            Log.e("PersonalityQuestion", "Error loading questions", e);
+            Log.e("PersonalityQuestion", "Error loading dummy questions", e);
         }
 
         return questions;
+    }
+
+    /**
+     * 백엔드에서 질문 데이터를 받아오는 메서드 (구현 예정)
+     */
+    public static List<PersonalityQuestion> loadQuestionsFromAPI() {
+        // TODO: 백엔드 API 호출로 질문 데이터 받아오기
+        /*
+        ApiService apiService = RetrofitClient.getApiService();
+        Call<List<PersonalityQuestion>> call = apiService.getPersonalityQuestions();
+        
+        try {
+            Response<List<PersonalityQuestion>> response = call.execute();
+            if (response.isSuccessful() && response.body() != null) {
+                return response.body();
+            }
+        } catch (IOException e) {
+            Log.e("PersonalityQuestion", "Error loading questions from API", e);
+        }
+        */
+        
+        // 실패시 빈 리스트 반환 (호출하는 쪽에서 더미 데이터로 대체)
+        return new ArrayList<>();
     }
 } 
