f8911025723aad999f5abb1138e9c795e4299af1
seo003 <sy030212@naver.com>
2025-08-17 22:01:40 +0900

    feat: 로그인 기능 구현

---
+++ app/src/main/java/com/example/teamup/api/ApiService.java
@@ -0,0 +1,20 @@
+package com.example.teamup.api;
+
+import com.example.teamup.api.model.UserDTO;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+/**
+ * FastAPI 서버와 통신하기 위한 Retrofit API 인터페이스
+ */
+public interface ApiService {
+    
+    /**
+     * 사용자 로그인 API
+     * POST /api/v1/users/login
+     */
+    @POST("api/v1/users/login")
+    Call<UserDTO> login(@Body UserDTO loginRequest);
+}
